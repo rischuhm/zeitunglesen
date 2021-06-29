4bc62e78380af1ce46301387e84a5f141200b71b
RichardSchuh <richard.schuhmann94@gmail.com>
2021-06-29 23:23:24 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+from requests_html import HTMLSession
+s = HTMLSession()
+r = s.get('https://www....')
+
+scripts = r.html.find('script')
+textfilter = list(filter(lambda script: "type" in script.attrs and script.attrs["type"] == "application/ld+json" and "articleBody" in script.text , scripts))
+text = textfilter[0].text
+
+text = json.loads(text)
+
+print(f'\n\033[1m{text["headline"]}\033[0m', end="\n\n")
+print(text["articleBody"])
