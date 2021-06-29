95ac02bfad49d741774745b2cc11fba6ebf0a75a
RichardSchuh <richard.schuhmann94@gmail.com>
2021-06-29 23:29:42 +0200

    changed url input

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 from requests_html import HTMLSession
 s = HTMLSession()
-r = s.get('https://www....')
+
+r = s.get(input("Please enter Url: "))
 
 scripts = r.html.find('script')
 textfilter = list(filter(lambda script: "type" in script.attrs and script.attrs["type"] == "application/ld+json" and "articleBody" in script.text , scripts))
